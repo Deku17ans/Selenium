454e5347cf92d5eb921c1bf04b04b02cdd949485
Motoya Ono <deked17ansducoton@gmail.com>
2019-05-04 23:42:45 +0900

    seleniumを用いてMoodleに自動ログインできるアルゴリズムを作りました

---
+++ git_MOODLE_auto_login.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\\Users\\onomo\\Documents\\Python\\Selenium\\chromedriver.exe')
+driver.get('https://moodle.cc.sophia.ac.jp/login/index.php')
+
+user = driver.find_element_by_name('username')
+user.send_keys('')
+login = driver.find_element_by_name('password')
+login.send_keys('')
+btn = driver.find_element_by_id('loginbtn')
+btn.click()
