43aac12246605cf4d91d333df4b1612d3bfaafc4
Motoya <43681290+Deku17ans@users.noreply.github.com>
2019-05-06 11:25:36 +0900

    Update git_MOODLE_auto_login.py

--- git_MOODLE_auto_login.py
+++ git_MOODLE_auto_login.py
@@ -4,8 +4,8 @@ driver = webdriver.Chrome('C:\\Users\\onomo\\Documents\\Python\\Selenium\\chrome
 driver.get('https://moodle.cc.sophia.ac.jp/login/index.php')
 
 user = driver.find_element_by_name('username')
-user.send_keys('')
+user.send_keys('ユーザー名')
 login = driver.find_element_by_name('password')
-login.send_keys('')
+login.send_keys('パスワード')
 btn = driver.find_element_by_id('loginbtn')
 btn.click()
