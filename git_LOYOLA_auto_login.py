fc50afdf7c3b5b1fc58f238b0b803db220f5ba65
Motoya <43681290+Deku17ans@users.noreply.github.com>
2019-05-06 11:26:38 +0900

    Update git_LOYOLA_auto_login.py

--- git_LOYOLA_auto_login.py
+++ git_LOYOLA_auto_login.py
@@ -4,12 +4,9 @@ import time
 driver = webdriver.Chrome('C:\\Users\\onomo\\Documents\\Python\\Selenium\\chromedriver.exe')
 driver.get('https://scs.cl.sophia.ac.jp/campusweb/campusportal.do')
 
-#driver.set_window_size()
-
 user_name = driver.find_element_by_name('userName')
-#time.sleep(3)
-user_name.send_keys('')
+user_name.send_keys('ユーザー名')
+
 password = driver.find_element_by_name('password')
-#time.sleep(3)
-password.send_keys('')
+password.send_keys('パスワード')
 password.submit()
